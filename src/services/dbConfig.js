246201f74eae43d1fe2c9efe81f1db0591f2bfaa
petkolatev <petkorlatev@gmail.com>
2025-03-31 21:01:37 +0300

    add origin CORS

--- src/services/dbConfig.js
+++ src/services/dbConfig.js
@@ -7,7 +7,7 @@ const config = {
     development: {
         port: process.env.PORT || 3000,
         dbURL: process.env.DB_URL || "mongodb://localhost:27017/books",
-        origin: ['http://localhost:5173']
+        origin: ['http://localhost:5173','http://localhost:4173']
     },
     production: {
         port: process.env.PORT || 3000,
